9cd2394ac54f3da28213a95cd7a3ae28a24ce18a
Brenner Shepherd <brenner.s985@stu.nebo.edu>
2019-10-25 10:29:08 -0600

    Start of spawn manager

---
+++ Personal Project/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float horizontalInput;
+    public GameObject projectile;
+    private float xRange = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position.x < -40 )
+       {
+           transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > 40)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        // get the user's vertical input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        
+    }
+}
---
+++ Personal Project/Assets/Challenge 1/Scripts/SpawnManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] planePrefabs;
+
+    private float spawnPosZ = 50;
+    private float spawnRangeX = 40;
+    //private float spawnLimitXLeft = -40;
+    //private float spawnLimitXRight = 40;
+
+
+    private float startDelay = 2;
+    private float spawnInterval = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Invoke("SpawnRandomPlane", startDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int planeIndex = Random.Range(0, planePrefabs.Length);
+            Instantiate(planePrefabs[planeIndex], spawnPos,
+            planePrefabs[planeIndex].transform.rotation);
+        }
+    }
+}
+
+
+
+
+
+//int planeIndex = Random.Range(0, planePrefabs.Length);
+//Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosZ, 0);
+//Invoke("SpawnRandomPlane", Random.Range(3, 5));
